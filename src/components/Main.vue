e097f07d56114a4b7ad804259551c267cb5edd36
SeungKi Kim <tttuuu888@gmail.com>
2019-11-16 20:40:37 +0900

    update

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  components: {
+  },
+  methods: {
+  },
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      msg2: 'test'
+    }
+  },
+  created () {
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
