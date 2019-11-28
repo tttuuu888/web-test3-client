9af4ba26260c819f38df7776853802ee783adb9c
SeungKi Kim <tttuuu888@gmail.com>
2019-11-29 02:35:01 +0900

    Add public addr seeting, post lists

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
     <input type="password" class="form-control" placeholder="PW" v-model="userPw">
     <button class="btn btn-success" v-on:click="loginUser">Login</button>
     <button class="btn btn-success" v-on:click="addUser">Sign-up</button>
+    <ui id="postList">
+      <li v-for="post in posts" :key="post">
+        {{ post }}
+      </li>
+    </ui>
   </div>
 </template>
 
@@ -17,10 +22,17 @@ export default {
     return {
       userId: '',
       userPw: '',
+      posts: ['none1', 'none2', 'none3'],
     }
   },
   mounted () {
     console.log('test component mounted.')
+    this.posts = ['mount1','mount2','mount3', ]
+    this.$http.get('/list').then( (result) => {
+      console.log(result.data.list)
+      this.posts = result.data.list
+    });
+
   },
   components: {
     Main
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  baseUrl: '/',
+  // baseUrl: '/',
   devServer: {
     proxy: {
       '/': {
         target:'http://localhost:4242',
         changeOrigin: true,
       },
-      '/test2': {
-        target:'http://localhost:4242/test2',
-        changeOrigin: true,
-      },
+      // '/add-user': {
+      //   target:'http://localhost:4242//add-user',
+      //   changeOrigin: true,
+      // },
     },
+    // public: 'test3.sktestpage.kro.kr',
   },
   publicPath: ''
   // publicPath: 'http://192.168.0.1:4242'
