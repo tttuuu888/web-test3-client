6707e4883290e8ec1a739b0eb0c2f1961bd1b9c8
SeungKi Kim <tttuuu888@gmail.com>
2019-12-03 01:30:20 +0900

    Add signup modal

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -3,41 +3,29 @@
     <form v-on:submit.prevent>
       <input placeholder="Enter your ID" v-model="userId">
       <input placeholder="Enter your password" type="password" v-model="userPw">
-      <button class="sumit" @click="loginUser">Login</button>
-      <button class="sumit" @click="addUser">Sign-up</button>
+      <button class="sumit" @click="loginClick">Login</button>
+      <button class="sumit" @click="signupClick">Sign-up</button>
     </form>
+    <SignupForm v-if="signupVisible" v-on:closeModal="closeModal" v-on:submitSignup="submitSignup"/>
   </div>
 </template>
 
 <script>
+import SignupForm from '@/views/SignupForm.vue'
+
 export default {
   name: 'loginform',
   components: {
+    SignupForm
   },
   methods: {
-    addUser: function() {
-      this.$http.post('/user/add',
-                      {
-                        auth: {
-                          id: 'test',
-                          name: 'tname',
-                          nickname: 'nick name',
-                          email: 'teste@mail.com',
-                          password: '1234'
-                        },
-                      },
-      ).then( (result) => {
-        console.log(result.data)
-        console.log("test api1 : " + result.data)
-      })
-    },
-    loginUser: function() {
-      console.log("login user,",this.userId,this.userPw)
+    login: function(id, pw) {
+      console.log("login user,",id, pw)
       this.$http.post('/user/login',
                       {
                         auth: {
-                          id: this.userId,
-                          password: this.userPw
+                          id: id,
+                          password: pw
                         },
                       },
       ).then( (result) => {
@@ -46,12 +34,26 @@ export default {
         console.log("loging id, nickname : " + loginId, loginNickname)
         this.$emit('loginUpdate', loginId, loginNickname)
       })
+    },
+    signupClick: function() {
+      this.signupVisible = true;
+    },
+    loginClick: function() {
+      this.login(this.userId,this.userPw);
+    },
+    closeModal: function() {
+      this.signupVisible = false;
+    },
+    submitSignup: function(user) {
+      console.log("user : " + user)
+      this.signupVisible = false;
     }
   },
   data () {
     return {
       userId: '',
       userPw: '',
+      signupVisible: false,
     }
   },
   created () {
